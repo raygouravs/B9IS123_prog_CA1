dd7045d3c0a4b168e4c1f5cea3e13f201f986f9b
gouravray <gourav.sankar93@gmail.com>
2025-10-31 00:00:56 +0000

    scaffolding completed. landing page created.

---
+++ client/src/components/login/landing.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import "./landing.css";
+
+function Login() {
+  return (
+    <>
+        <div className="login-container">
+          <h1>Login</h1>
+          <form>
+            <div>
+              <label htmlFor="username">Username:</label>
+              <input type="text" id="username" name="username" required />
+            </div>
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+    </>
+  )
+}
+
+export default Login
