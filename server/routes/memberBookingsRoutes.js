2f25f10ffc310bd7f20a62ef7095597d17766717
gouravray <gourav.sankar93@gmail.com>
2025-11-23 22:28:21 +0000

    route added for getAllBookings

--- server/routes/memberBookingsRoutes.js
+++ server/routes/memberBookingsRoutes.js
@@ -5,5 +5,8 @@ const bookingsController = require('../controllers/memberBookingsController');
 // POST /api/bookings/create
 router.post('/create', bookingsController.createBooking);
 
+// GET /api/bookings/all
+router.get('/all', bookingsController.getAllBookings);
+
 
 module.exports = router;
