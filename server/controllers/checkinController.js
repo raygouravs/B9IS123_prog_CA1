3895b3f4198d7213404057beee98aea9b7014ff7
gouravray <gourav.sankar93@gmail.com>
2025-11-27 00:25:39 +0000

    added controller method for create checkin endpoint

--- server/controllers/checkinController.js
+++ server/controllers/checkinController.js
@@ -1,1 +1,14 @@
+const Checkins = require('../models/checkinModel');
 
+module.exports = {
+    createCheckin(req, res) {
+        try {
+            const checkin = req.body.checkin;
+            const result = Checkins.createCheckin(checkin);
+            res.json(result);
+        } catch (error) {
+            console.error("Error creating checkin:", error);
+            res.status(500).json({ error: 'Failed to create checkin!' });
+        }
+    }
+}
