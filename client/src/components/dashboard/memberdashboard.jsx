46b6f2e561d42d35c81e8dedcbc8ffda199774bb
gouravray <gourav.sankar93@gmail.com>
2025-12-10 14:14:04 +0000

    added the navbar to the membr landing page

--- client/src/components/dashboard/memberdashboard.jsx
+++ client/src/components/dashboard/memberdashboard.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import "./memberdashboard.css";
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import Navbar from '../navbar/navbar';
 
 function MemberDashboard() {
 
   const navigate = useNavigate();
   
   return (
-    <>
+    <>  
+        <Navbar title="Hybrid Workspace Management System" />
         <div className="dashboard-container">
           <h2>Welcome to the Member Portal, {localStorage.getItem("full_name")}</h2>
       
