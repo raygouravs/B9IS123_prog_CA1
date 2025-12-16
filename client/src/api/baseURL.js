4d0d49dfbf99c2c50be0dd65a80d6008e3b76834
gouravray <gourav.sankar93@gmail.com>
2025-12-16 12:27:45 +0000

    base URL changed to new public IP

--- client/src/api/baseURL.js
+++ client/src/api/baseURL.js
@@ -3,6 +3,6 @@
 
 // base URL for EC2 deployment
 // export const API_BASE_URL = "http://<EC2-public-IP>:3000/api";
-export const API_BASE_URL = "http://52.19.157.50:3000/api";
+export const API_BASE_URL = "http://34.247.66.21:3000/api";
 // NOTE: update the <EC2-public-IP> everytime the EC2 reboots!
 
