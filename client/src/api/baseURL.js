389cc468e5242a81ec5abbee5e8baf134c260013
gouravray <gourav.sankar93@gmail.com>
2025-12-16 11:39:03 +0000

    updared the public IP of the rebooted EC2

--- client/src/api/baseURL.js
+++ client/src/api/baseURL.js
@@ -3,6 +3,6 @@
 
 // base URL for EC2 deployment
 // export const API_BASE_URL = "http://<EC2-public-IP>:3000/api";
-export const API_BASE_URL = "http://108.130.128.164:3000/api";
+export const API_BASE_URL = "http://52.19.157.50:3000/api";
 // NOTE: update the <EC2-public-IP> everytime the EC2 reboots!
 
